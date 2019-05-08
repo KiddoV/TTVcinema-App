21690a1de700b95accedce9a290e1d7895504c69
Thomas Kok <kokthomas7016@gmail.com>
2019-05-08 12:49:55 -0400

    git ignore the apiKey

--- src/app/components/search-movie/search-movie.component.ts
+++ src/app/components/search-movie/search-movie.component.ts
@@ -13,7 +13,6 @@ import { SearchMovieService } from 'src/app/services/search-movie.service';
 })
 export class SearchMovieComponent implements OnInit {
 
-  // movieTitle="";
  
   movieNameInput: String;
   searchResult: any;  //This value to hold the search result @Viet
@@ -22,28 +21,6 @@ export class SearchMovieComponent implements OnInit {
   constructor(public searchMovieService: SearchMovieService) {}
 
   ngOnInit() {}
-
-  // fetchData() { 
-  //   this.http.get("https://api.themoviedb.org/3/search/movie?api_key=e889b3766f72d291bb10bbfed659982c&language=en-US&query="
-  //    + this.movieTitle
-  //    + "&page=1&include_adult=false").subscribe(
-  //     data=> {
-  //       console.log(data);
-  //       let movies = data["results"];
-        
-  //       for(let index in movies) {
-  //         let row = document.getElementById("movieTable");
-  //         let tr = document.createElement("tr");
-  //         tr.insertCell(0).innerText = movies[index]["title"];
-  //         tr.insertCell(1).innerText = movies[index]["overview"];
-  //         row.appendChild(tr);
-  //       }
-  //     },
-  //     error=>{
-  //       console.log(error);
-  //     }
-  //    );
-  // }
 
   getMovies() {
     this.searchMovieService.searchMovie(this.movieNameInput).subscribe(data => {
